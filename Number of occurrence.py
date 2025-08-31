5c5f81693f569e4aaa1138f0c6fd1a7a9221c476
Amar Manjarathkar <157588083+Amar-Manjarathkar@users.noreply.github.com>
2025-08-31 20:48:06 +0530

    Create Number of occurrence.py
    Given a sorted array, arr[] and a number target, you need to find the number of occurrences of target in arr[].

---
+++ Number of occurrence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countFreq(self, arr, target):
+        # code here
+        count = 0
+        for num in arr:
+            if num == target:
+                count+=1
+        return count
+        
