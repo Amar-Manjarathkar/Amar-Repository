d7c0d89a5b43835547bd9d0f2bf6add709ce0494
Amar Manjarathkar <157588083+Amar-Manjarathkar@users.noreply.github.com>
2025-09-02 22:25:42 +0530

    Update Parenthesis Checker.py
    optimized query

--- Parenthesis Checker.py
+++ Parenthesis Checker.py
@@ -1,13 +1,5 @@
 class Solution:
-    def isBalanced(self, s: str) -> bool:
-        stack = []
-        pairs = {')':'(', '}':'{', ']':'['}
-        
-        for ch in s:
-            if ch in pairs.values():   # opening
-                stack.append(ch)
-            elif ch in pairs:          # closing
-                if not stack or stack[-1] != pairs[ch]:
-                    return False
-                stack.pop()
-        return not stack
+    def isBalanced(self, s):
+        while "()" in s or "{}" in s or "[]" in s:
+            s = s.replace("()", "").replace("{}", "").replace("[]", "")
+        return s == ""
