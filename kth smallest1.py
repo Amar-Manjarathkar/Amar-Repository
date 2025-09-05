65ce749b2a7e322799db0135a222b7d73e638b83
Amar Manjarathkar <157588083+Amar-Manjarathkar@users.noreply.github.com>
2025-09-05 22:26:53 +0530

    Create kth smallest1.py
    using bubble sort (Time limit exceeded error)

---
+++ kth smallest1.py
@@ -0,0 +1,15 @@
+#User function Template for python3
+
+
+class Solution:
+
+    def kthSmallest(self, arr,k):
+        n = len(arr)
+        
+        for i in range(n):
+            for j in range(0,n-i-1):
+                if arr[j] > arr[j+1]:
+                    arr[j],arr[j+1]=arr[j+1],arr[j]
+        return arr[k-1]
+        
+        
