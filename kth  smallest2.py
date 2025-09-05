2dea6f5193521d9c6a81f5c0303047cd0eb4ae79
Amar Manjarathkar <157588083+Amar-Manjarathkar@users.noreply.github.com>
2025-09-05 22:31:59 +0530

    Create kth  smallest2.py
    with insertion sort (time limit exceeded)

---
+++ kth  smallest2.py
@@ -0,0 +1,18 @@
+#User function Template for python3
+
+
+class Solution:
+
+    def kthSmallest(self, arr,k):
+        n = len(arr)
+        
+        for i in range(1, len(arr)):
+            key = arr[i]
+            j = i - 1
+            while j >= 0 and key < arr[j]:
+                arr[j + 1] = arr[j]
+                j -= 1
+            arr[j + 1] = key
+        return arr[k-1]
+        
+        
