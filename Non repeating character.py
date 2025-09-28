6ddef6daa25db0cab347e5341d0c79ab8ef48972
Amar Manjarathkar <157588083+Amar-Manjarathkar@users.noreply.github.com>
2025-09-28 19:59:24 +0530

    Create Non repeating character.py
    Given a string s consisting of lowercase English Letters. return the first non-repeating character in s. If there is no non-repeating character, return '$'.

---
+++ Non repeating character.py
@@ -0,0 +1,14 @@
+class Solution:
+    def nonRepeatingChar(self,s):
+        #code here
+        frequency_map = {}
+        
+        for char in s:
+            frequency_map[char] = frequency_map.get(char,0)+1
+            
+        for char in s:
+            if frequency_map[char] == 1:
+                return char
+        return '$'
+    
+    
